e5a271a186c2905f90a92f7c3d2bb57671ef78d1
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-08-17 13:48:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+let num1 = 1;
+let num2 = 62;
+let multiply = (num1*num2);
+console.log(multiply);
+
+let num3 = 25;
+let num4 = 7;
+let mod = (num3%num4);
+console.log(mod)
+
+let random = Math.floor(Math.random()*10);
+
+let max = Math.max(5, 6, 8, 9, 14, 15, 20 );
+
